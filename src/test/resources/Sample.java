32f9dd92937bd9b4dce9647b3e4fc85191de4d31
Jenifer0498 <jeniferramasamy004@gmail.com>
2023-01-24 18:11:42 +0530

    first method

---
+++ src/test/resources/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+private void tc1() {
+	// TODO Auto-generated method stub
+
+}private void tc2() {
+	// TODO Auto-generated method stub
+
+}private void tc3() {
+	// TODO Auto-generated method stub
+
+}
+}
